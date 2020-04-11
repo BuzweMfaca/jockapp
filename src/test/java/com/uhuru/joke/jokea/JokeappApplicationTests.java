6c09853e7287ef3ddaeed3a23a6b960445102488
Buzwe Mfaca <bmfaca@gmail.com>
2020-04-11 17:25:37 +0200

    Initial commit

---
+++ src/test/java/com/uhuru/joke/jokea/JokeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uhuru.joke.jokea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JokeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
